561d3774d30c6962f493549cadf3fb24b4609d71
12Me21 <12me21.mc@gmail.com>
2020-05-25 20:30:19 -0400

    change script order

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 echo "" > build.html
 sed '/<!--START-->/Q' index.html >> build.html
 echo "<script>" >> build.html
-cat event.js storage.js myself.js lp.js sbhighlight.js 12y.js bbcode.js render.js main.js >> build.html
+cat event.js storage.js myself.js lp.js render.js main.js sbhighlight.js 12y.js bbcode.js >> build.html
 echo "</script>" >> build.html
 echo "<style>" >> build.html
 cat style.css markup.css code.css >> build.html
