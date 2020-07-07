d50a4bd0ec7e2fd3b7656dd2c46bd80e23f046d3
12Me21 <12me21.mc@gmail.com>
2020-07-07 18:09:29 -0400

    remove storage.js from build script

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cat fonts.css style.css markup.css code.css >> build.html
 echo "</style>" >> build.html
 
 echo "<script>" >> build.html
-cat util.js storage.js myself.js lp4.js sbhighlight.js parse.js render.js view.js events.js main.js >> build.html
+cat util.js myself.js lp4.js sbhighlight.js parse.js render.js view.js events.js main.js >> build.html
 echo "</script>" >> build.html
 
 sed '1,/<!--END-->/d' index.html >> build.html
